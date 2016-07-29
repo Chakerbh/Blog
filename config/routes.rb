61a3bea34450553846fbc8103a45f61162d5eacf
Chaker <chaker.benhamed@gmail.com>
2016-07-29 14:46:18 +0100

    Changing authentication routes, adding style

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
-  
-  # authenticated :user do
-  #   resources :posts
-  # end
+
   resources :posts
 
-  unauthenticated :user do
-       resources :posts, only: [:show, :index]
-  end
-
-
-  root 'posts#index' 
+  root 'posts#index'
 end
