f82da977338e8e00ae4262c7e7063a7e5341fc54
Chaker <chaker.benhamed@gmail.com>
2016-08-06 17:52:20 +0100

    Sort post DESC by creation date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
-  validates :title, :body, presence: true
+
+  default_scope  { order(:created_at => :desc) }
+
   scope :drafts, -> { where(draft: true) }
   scope :published, -> { where(draft: false) }
+
+  validates :title, :body, presence: true
 end
